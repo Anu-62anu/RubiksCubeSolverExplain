989d69cb5247077aef23b7c30d7293fc67115e3a
Anusha Kurra <kuraanusha625@gmail.com>
2024-08-13 16:58:47 +0530

    Added GenericRubiksCube Model

---
+++ GenericRubiksCube.cpp
@@ -0,0 +1,5 @@
+//
+// Created by KURRA ANUSHA on 13-08-2024.
+//
+
+#include "GenericRubiksCube.h"
---
+++ GenericRubiksCube.h
@@ -0,0 +1,16 @@
+//
+// Created by KURRA ANUSHA on 13-08-2024.
+//
+
+#ifndef GENERICRUBIKSCUBE_H
+#define GENERICRUBIKSCUBE_H
+
+
+
+class GenericRubiksCube {
+
+};
+
+
+
+#endif //GENERICRUBIKSCUBE_H
